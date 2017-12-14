0852effb237dd180245173ac50b22c1b4970020c
jscricket <jscricket@exef.io>
2017-12-14 23:01:15 +0200

    Created template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
+// import { AgmCoreModule } from '@agm/core';
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './component/weather/weather.component';
+import { HeaderComponent } from './component/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
 		WeatherComponent,
+		HeaderComponent,
 	
   ],
   imports: [
 		BrowserModule,
 		CommonModule,
-		FormsModule,
-		AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyC0ASewGAO45_lcnKbjPvurmuGZAQ41NCY'
-    })
+		FormsModule
+		// AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyAo1l-p9UldY_oSrkJMtUzKa2_QLdtTnpY'
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
